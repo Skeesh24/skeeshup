f9d96cb996559dea85d265d14eb2859ddbe900b7
skeesh24 <d.pacooloff@gmail.com>
2023-12-24 23:55:02 +0500

    refactoring main

--- main.py
+++ main.py
@@ -1,39 +1,60 @@
 import asyncio
+from datetime import datetime
+from hashlib import sha256
+from os import mkdir
+from os.path import exists
 
 from methods import script
-from settings import conf, logger
+from settings import conf, env, logger
 
 
 async def download_binary(download_args: list) -> None:
     ps = conf["PS"]
-    logger.debug("downloading with the args" + str(download_args))
+
+    logger.debug("downloading with the args: " + str(download_args))
     await script(ps["DIRECTORY"] + ps["DOWNLOAD_FILE"], download_args)
 
 
-async def install_binary(install_args: list) -> None:
+async def install_binary(args: str) -> None:
     ps = conf["PS"]
-    logger.debug("installing with the args" + str(install_args))
-    await script(ps["DIRECTORY"] + ps["INSTALL_FILE"], install_args)
+    logger.debug("installing with the args: " + str(args))
+    await script(ps["DIRECTORY"] + ps["INSTALL_FILE"], args)
 
 
-async def main_process(download_args_array: list, install_args_array: list) -> None:
-    while len(download_args_array) != 0 or len(install_args_array) != 0:
-        download_args = download_args_array.pop()
-        install_args = install_args_array.pop()
+async def delete_binary(args: list) -> None:
+    ps = conf["PS"]
+    logger.debug("deletion with the args: " + str(args))
+    await script(ps["DIRECTORY"] + ps["DELETE_FILE"], args)
+
+
+def get_temp_location() -> str:
+    path = f"{env.TEMP}\\win\\"
+    if not exists(path):
+        mkdir(path)
+    return path
+
+
+async def installation_process(download_args_array: list) -> None:
+    while len(download_args_array) != 0:
+        location = get_temp_location()
+        filename = location + f"{sha256(str(datetime.now()).encode()).hexdigest()}.exe"
+
         logger.info("starting download")
-        await download_binary(download_args)
+        await download_binary([download_args_array.pop(), filename])
 
         logger.info("starting instalation")
-        await install_binary(install_args)
+        await install_binary([filename])
+
+    logger.info("starting cleanup temp directory")
+    await delete_binary([location])
 
 
-async def main(): 
+async def main():
     logger.info("program started")
-    download_args_array: list = conf["CONTENT"]["DOWNLOAD_ARGS_ARRAY"]
-    install_args_array: list = conf["CONTENT"]["INSTALL_ARGS_ARRAY"]
+    download_args: list = conf["CONTENT"]["DOWNLOAD_ARGS"]
 
     try:
-        await main_process(download_args_array, install_args_array)
+        await installation_process(download_args)
 
     except Exception as e:
         logger.error("there is an error in the program runtime :(" + str(e))
