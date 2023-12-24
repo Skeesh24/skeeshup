9c23b7843ba5ad5b1d47de2c8d95d4c3477ae44f
skeesh24 <d.pacooloff@gmail.com>
2023-12-24 14:24:26 +0500

    refactoring settings: extracting methods and simplifying

--- settings.py
+++ settings.py
@@ -13,17 +13,11 @@ class Settings:
         """
         Downloads the .env file into the dotenv field as a dictionary
         """
-        self.dotenv = dotenv_values()
+        load_dotenv()
 
-    def __getitem__(self, key: str) -> str:
+    def __getattribute__(self, key: str) -> str:
         """
-        Returns a .env variable value or an empty string from a dotenv field
-        """
-        return self.dotenv.get(key, "")
-
-    def from_env(self, key: str) -> str:
-        """
-        Returns a .env variable value or an empty string from a environment variables list
+        Returns a .env variable value or an empty string from environment variables list
         """
         return environ.get(key, "")
 
@@ -31,26 +25,26 @@ class Settings:
 def build_logging() -> None:
     global logger
     logger = getLogger(__name__)
-    logger.setLevel(int(env["LOG_LEVEL"]))
-    basicConfig(format=env["LOG_FORMAT"])
+    logger.setLevel(int(env.LOG_LEVEL))
+    basicConfig(format=env.LOG_FORMAT)
 
 
-def build_system_environment() -> None:
-    """
-    Downloads the .env file into the environment variables list
-    """
-    load_dotenv()
-
-
-def build_configuration() -> None:
-    return load(env[''])
+def build_configuration(path: str = "") -> Dict[str, str]:
+    try:
+        logger.debug(
+            "loading confiration file at " + path
+            if path != ""
+            else env.CONFIGURATION_PATH
+        )
+        return load(open(env.CONFIGURATION_PATH)) if path == "" else load(open(path))
+    except Exception as e:
+        logger.error("error while loading configuration file: " + str(e))
 
 
 def configure() -> None:
     global env
     env = Settings()
     build_logging()
-    build_system_environment()
 
 
 configure()
