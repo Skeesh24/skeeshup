d6fb7dff2f6a55398089b23c397db7bb03a05660
skeesh24 <d.pacooloff@gmail.com>
2023-12-24 16:41:12 +0500

    refactoring main with the looging

--- main.py
+++ main.py
@@ -4,26 +4,36 @@ from methods import script
 from settings import conf, logger
 
 
-async def main():
-    logger.info("program started")
-    logger.debug("getting the configuration")
-    content = conf["CONTENT"]
+async def download_binary(download_args: list) -> None:
     ps = conf["PS"]
-    download_args_array: list = content["DOWNLOAD_ARGS_ARRAY"]
-    install_args_array: list = content["INSTALL_ARGS_ARRAY"]
+    logger.debug("downloading with the args" + str(download_args))
+    await script(ps["DIRECTORY"] + ps["DOWNLOAD_FILE"], download_args)
 
-    try:
+
+async def install_binary(install_args: list) -> None:
+    ps = conf["PS"]
+    logger.debug("installing with the args" + str(install_args))
+    await script(ps["DIRECTORY"] + ps["INSTALL_FILE"], install_args)
+
+
+async def main_process(download_args_array: list, install_args_array: list) -> None:
+    while len(download_args_array) != 0 or len(install_args_array) != 0:
+        download_args = download_args_array.pop()
+        install_args = install_args_array.pop()
         logger.info("starting download")
-        while not len(download_args_array) == 0:
-            download_args = download_args_array.pop()
-            logger.debug("downloading with the args" + str(download_args))
-            await script(ps["DIRECTORY"] + ps["DOWNLOAD_FILE"], download_args)
+        await download_binary(download_args)
 
         logger.info("starting instalation")
-        while not len(install_args_array) == 0:
-            install_args = install_args_array.pop()
-            logger.debug("installing with the args" + str(install_args))
-            await script(ps["DIRECTORY"] + ps["INSTALL_FILE"], install_args)
+        await install_binary(install_args)
+
+
+async def main():
+    logger.info("program started")
+    download_args_array: list = conf["CONTENT"]["DOWNLOAD_ARGS_ARRAY"]
+    install_args_array: list = conf["CONTENT"]["INSTALL_ARGS_ARRAY"]
+
+    try:
+        await main_process(download_args_array, install_args_array)
 
     except Exception as e:
         logger.error("there is an error in the program runtime :(" + str(e))
