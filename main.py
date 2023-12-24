93787972443bc51c0d28e594def67a8b7dfe1d39
skeesh24 <d.pacooloff@gmail.com>
2023-12-24 16:26:24 +0500

    cycling downloading and installating

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import asyncio
 
 from methods import script
-from settings import logger, conf
+from settings import conf, logger
 
 
 async def main():
@@ -9,10 +9,22 @@ async def main():
     logger.debug("getting the configuration")
     content = conf["CONTENT"]
     ps = conf["PS"]
+    download_args_array: list = content["DOWNLOAD_ARGS_ARRAY"]
+    install_args_array: list = content["INSTALL_ARGS_ARRAY"]
 
     try:
-        await script(ps["DIRECTORY"] + ps["DOWNLOAD_FILE"], content["DOWNLOAD_ARGS"])
-        await script(ps["DIRECTORY"] + ps["INSTALL_FILE"], content["INSTALL_ARGS"])
+        logger.info("starting download")
+        while not len(download_args_array) == 0:
+            download_args = download_args_array.pop()
+            logger.debug("downloading with the args" + str(download_args))
+            await script(ps["DIRECTORY"] + ps["DOWNLOAD_FILE"], download_args)
+
+        logger.info("starting instalation")
+        while not len(install_args_array) == 0:
+            install_args = install_args_array.pop()
+            logger.debug("installing with the args" + str(install_args))
+            await script(ps["DIRECTORY"] + ps["INSTALL_FILE"], install_args)
+
     except Exception as e:
         logger.error("there is an error in the program runtime :(" + str(e))
 
