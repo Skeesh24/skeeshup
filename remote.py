804ae18a5925393bc441e349bd2fbcd7674b52ed
skeesh24 <d.pacooloff@gmail.com>
2023-12-25 15:40:18 +0500

    extracting mongo integration

--- remote.py
+++ remote.py
@@ -12,10 +12,14 @@ URI = "mongodb+srv://%s:%s@%s" % (
 )
 
 def get_remote_configuration() -> Dict:
-    client = MongoClient(URI)
-    db = client.get_database(env.MONGO_DATABASE)
-    collection = db.get_collection(env.MONGO_COLLECTION)
+    collection = collection_connect()
     conf = collection.find_one()
     if conf is None or conf == {}:
         raise FileNotFoundError("There is no cloud configuration saved")
     return conf
+
+def collection_connect():
+    client = MongoClient(URI)
+    db = client.get_database(env.MONGO_DATABASE)
+    collection = db.get_collection(env.MONGO_COLLECTION)
+    return collection
