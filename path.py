6929e612bed44d7895854355a23c65d0bf193c02
skeesh24 <d.pacooloff@gmail.com>
2023-12-26 16:40:04 +0500

    add docstrings and a new env var for folder path

--- path.py
+++ path.py
@@ -7,16 +7,25 @@ from settings import env
 
 
 def get_temp_location() -> str:
-    path = f"{env.TEMP}\\win\\"
+    """
+    Returns an absolute path to the temp download folder 
+    """
+    path = f"{env.TEMP}\\{env.TEMP_FOLDER}\\"
     if not exists(path):
         mkdir(path)
     return path
 
 
 def rand_hash() -> str:
+    """
+    Generates a new sha256 hash by the current time
+    """
     return sha256(str(datetime.now()).encode()).hexdigest()
 
 
 def get_dir_and_filename() -> (str, str):
+    """
+    Returns a new random name for the temporary file 
+    """
     location = get_temp_location()
     return location, location + f"{rand_hash()}.exe"
