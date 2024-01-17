df0acff85f2da971d586d89b9b12008f61233eac
skeesh24 <d.pacooloff@gmail.com>
2024-01-17 17:01:37 +0500

    adjust the var name, extract the methods to processing package

--- src/main.py
+++ src/main.py
@@ -1,52 +1,16 @@
 import asyncio
-from os.path import abspath, curdir
-
-from path import get_dir_and_filename
-from scripting import download_bytes, install_binary, remove_item, unzip_archive
+from processing import archive_process, installation_process
 from settings import conf, env, logger
-
-
-async def installation_process(download_args: list) -> None:
-    try:
-        while len(download_args) != 0:
-            location, filename = get_dir_and_filename()
-
-            logger.info("starting download")
-            await download_bytes([download_args.pop(), filename])
-
-            logger.info("starting instalation")
-            await install_binary([filename])
-    except (KeyboardInterrupt, SystemExit):
-        logger.error("interrupted, rolling back changes")
-    finally:
-        logger.info("starting cleanup temp directory")
-        await remove_item([location])
-
-
-async def archive_process(archive_args: str) -> None:
-    try:
-        logger.info("downloading archive from a cloud")
-        curdirabs = abspath(curdir)
-        archive_name = curdirabs + "\\skeeshup-archive.rar"
-        await download_bytes([archive_args, archive_name])
-        logger.info("unziping archive")
-        await unzip_archive(curdirabs, archive_name)
-
-    except (KeyboardInterrupt, SystemExit):
-        logger.error("interrupted,  rolling back changes")
-    finally:
-        logger.info("starting cleanup an archive")
-        await remove_item([archive_name])
 
 
 async def main():
     logger.info("program started")
     logger.info("getting download configuration. type: " + env.SYNC)
     installation_args: list = conf["CONTENT"]["INSTALLATION_ARGS"]
-    archive_args: str = conf["CONTENT"]["ARCHIVE_ARGS"]
+    archive_link: str = conf["CONTENT"]["ARCHIVE_ARGS"]
 
     await installation_process(installation_args)
-    await archive_process(archive_args)
+    await archive_process(archive_link)
 
 
 if __name__ == "__main__":
